904f01c2c2a7fcac7e0ac4bb15e079872ebf9eaa
andrewpixelgemium <andrewpixelrada@gmail.com>
2025-02-10 22:58:03 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.ibb.co", "d3i6fh83elv35t.cloudfront.net", "preview.redd.it", "i.ytimg.com", "hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+    domains: ["i.ibb.co", "d3i6fh83elv35t.cloudfront.net", "preview.redd.it", "i.ytimg.com", "hebbkx1anhila5yf.public.blob.vercel-storage.com", "images.freeimages.com"],
   },
 };
 
