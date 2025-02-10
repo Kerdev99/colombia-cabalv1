28f66a48437a9bdc8499ac5c780b0b338e9ac5c7
andrewpixelgemium <andrewpixelrada@gmail.com>
2025-02-10 22:53:37 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["i.ibb.co", "d3i6fh83elv35t.cloudfront.net", "preview.redd.it", "i.ytimg.com", "hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+  },
+};
 
 export default nextConfig;
